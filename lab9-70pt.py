5fc002036e1d446ab5ee9f096f80046545e049b0
LoganBlair <s430720@student.roundrockisd.org>
2014-10-17 14:57:19 -0500

    Commit, finished 70 pt

--- lab9-70pt.py
+++ lab9-70pt.py
@@ -12,3 +12,17 @@
 # Accept user input 
 # Calculate fahrenheit
 # Output answer
+
+print 'Enter the temperature in celcius and I will give you the temperature in fahrenheit'
+userinput = raw_input()
+userinput = int(userinput) * 9
+userinput = int(userinput) / 5
+userinput = int(userinput) + 35
+print userinput
+
+
+
+
+
+
+
